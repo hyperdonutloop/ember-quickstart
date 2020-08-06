78eac50476a338aaa861d6152ec4b00d28071899
donutwizard666 <thebeardedwarrior@icloud.com>
2020-08-06 15:32:18 -0700

    done with tutorial

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class ScientistsRoute extends Route {
   model() {
-    return ['Odin', 'Morti', 'Hypebeast', 'Helios']
+    return ['AllFather', 'Mortifer Nibblesworth', 'HypeBeast', 'Hel']
   }
 }
